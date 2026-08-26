b176d8c825fbf28a363f2f2eae3dec99e399496d
ahsananaseefa-source <ahsananaseefa@gmail.com>
2026-08-26 22:00:44 +0530

    Time: 0 ms (100%), Space: 55 MB (90.48%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let answer = [];
+
+    for(let i = 0 ; i < s.length; i++){
+        answer[indices[i]]= s[i];
+    }
+
+    return answer.join("")
+    
+};
