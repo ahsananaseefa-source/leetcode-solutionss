19d1a7ed14aec20e688e152d2f61e344b76c724b
ahsananaseefa-source <ahsananaseefa@gmail.com>
2026-09-04 09:33:08 +0530

    Time: 1 ms (23.93%), Space: 53.7 MB (88.23%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0;
+
+    while(n > 0){
+        count +=  n % 2;
+        n = Math.floor(n/2)
+    }
+    return count
+    
+};
