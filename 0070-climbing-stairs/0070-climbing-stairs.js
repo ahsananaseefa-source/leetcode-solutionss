295cfcf7283320cc03a9a6f827745bfb4505da32
ahsananaseefa-source <ahsananaseefa@gmail.com>
2026-09-07 09:33:25 +0530

    Time: 0 ms (100%), Space: 53.4 MB (65%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if(n <= 2){
+        return n;
+    }
+    let a = 1;
+    let b = 2;
+
+    for(let i =3; i <= n; i++){
+        let rslt = a + b;
+        a = b;
+        b = rslt
+    }
+    return b
+    
+
+    
+    
+};
