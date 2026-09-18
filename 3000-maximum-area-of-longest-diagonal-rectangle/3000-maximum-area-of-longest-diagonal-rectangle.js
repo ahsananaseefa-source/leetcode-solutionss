9b8a2005b7d41f12fe45d50bee31108ae1345e85
ahsananaseefa-source <ahsananaseefa@gmail.com>
2026-09-18 19:41:21 +0530

    Time: 1 ms (65%), Space: 56.2 MB (50%) - LeetHub

---
+++ 3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} dimensions
+ * @return {number}
+ */
+var areaOfMaxDiagonal = function(dimensions) {
+     let maxDiagonal = 0;
+    let maxArea = 0;
+
+    for (let i = 0; i < dimensions.length; i++) {
+
+        let length = dimensions[i][0];
+        let width = dimensions[i][1];
+
+        let diagonal = length * length + width * width;
+        let area = length * width;
+
+        if (diagonal > maxDiagonal) {
+            maxDiagonal = diagonal;
+            maxArea = area;
+        }
+
+        else if (diagonal === maxDiagonal && area > maxArea) {
+            maxArea = area;
+        }
+    }
+
+    return maxArea;
+    
+};
